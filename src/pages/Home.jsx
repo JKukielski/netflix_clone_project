aab68dbc5a35b406ee2c8b677b1b3dbe0d7e9550
JKukielski <jankukielski1@gmail.com>
2023-05-22 20:27:44 +0200

    firebase setup + user logic

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import Main from "../components/Main";
+import Row from "../components/Row";
+import requests from "./Request";
 
 const Home = () => {
   return (
     <>
       <Main />
+      <Row rowId="1" title="Upcoming" fetchURL={requests.requestUpcoming} />
+      <Row rowId="2" title="Popular" fetchURL={requests.requestPopular} />
+      <Row rowId="3" title="Trending" fetchURL={requests.requestTrending} />
+      <Row rowId="4" title="Top Rated" fetchURL={requests.requestTopRated} />
+      <Row rowId="5" title="Horror" fetchURL={requests.requestHorror} />
     </>
   );
 };
